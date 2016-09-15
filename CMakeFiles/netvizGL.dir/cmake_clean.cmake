922436611fbcd9b020a1c0e8e0661e65a09ca683
werl <werl@elitebook>
2016-09-15 14:04:26 +0100

    Environment set up

---
+++ CMakeFiles/netvizGL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/netvizGL.dir/main.cpp.o"
+  "netvizGL.pdb"
+  "netvizGL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/netvizGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
