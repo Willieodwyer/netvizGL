759b8b2536b4b2ba4a5e3964a7beda7fbe993ffb
werl <werl@elitebook>
2016-09-18 23:45:30 +0100

    I think ive it set up right now

--- main.cpp
+++ main.cpp
@@ -1,82 +1,91 @@
-#include <GL/glut.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cstdlib>
 
-void sendHELP()
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+int main( void )
 {
-  glClear(GL_COLOR_BUFFER_BIT); // clear the window
+    GLFWwindow *window;
 
-  // set line width, in pixels
-  glLineWidth(4.0);
+    // Initialize the library
+    if ( !glfwInit( ) )
+    {
+        return -1;
+    }
 
-  // Draw H
-  glBegin(GL_LINES);
-    glColor3f(1.0, 1.0, 1.0); // set color to white
-    glVertex3f(-0.8, -0.6 ,0.0);
-    glVertex3f(-0.8, 0.6, 0.0);
-    glVertex3f(-0.4, 0.6, 0.0);
-    glVertex3f(-0.4, -0.6, 0.0);
-    glVertex3f(-0.8, 0.0, 0.0);
-    glVertex3f(-0.4, 0.0, 0.0);
-  glEnd();
-
-  // Draw E
-  glBegin(GL_LINES);
-    glColor3f(1.0, 0.0, 0.0); // set color to red
-    glVertex3f(-0.3, -0.6 ,0.0);
-    glVertex3f(-0.3, 0.6, 0.0);
-    glVertex3f(-0.3, 0.6 , 0.0);
-    glVertex3f(0.0, 0.6 , 0.0);
-    glVertex3f(-0.3, 0.0 , 0.0);
-    glVertex3f(0.0, 0.0 , 0.0);
-    glVertex3f(-0.3, -0.6 , 0.0);
-    glVertex3f(0.0, -0.6 , 0.0);
-  glEnd();
-
-  //Draw L
-  glBegin(GL_LINES);
-    glColor3f(0.0, 1.0, 0.0); // set color to red
-    glVertex3f(0.1, -0.6 ,0.0);
-    glVertex3f(0.1, 0.6, 0.0);
-    glVertex3f(0.1, -0.6 , 0.0);
-    glVertex3f(0.4, -0.6 , 0.0);
-  glEnd();
-
-  //Draw P
-
-  glBegin(GL_LINES);
-    glColor3f(0.0, 0.0, 1.0); // set color to red
-    glVertex3f(0.5, 0.6 ,0.0);
-    glColor3f(1.0,1.0,0.0);
-    glVertex3f(0.9, 0.6, 0.0);
-    glColor3f(1.0,0.0,0.0);
-    glVertex3f(0.5, 0.0 , 0.0);
-    glVertex3f(0.9, 0.0 , 0.0);
-    glVertex3f(0.5, 0.6 , 0.0);
-    glColor3f(1.0,1.0,0.0);
-    glVertex3f(0.5, -0.6 , 0.0);
-    glVertex3f(0.9, 0.6 , 0.0);
-    glVertex3f(0.9, -0.0 , 0.0);
-  glEnd();
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow( SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL );
 
 
-  glFlush();
+
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize( window, &screenWidth, &screenHeight );
+
+    if ( !window )
+    {
+        glfwTerminate( );
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent( window );
+
+    glViewport( 0.0f, 0.0f, screenWidth, screenHeight ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+    glMatrixMode( GL_PROJECTION ); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
+    glLoadIdentity( ); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
+    glOrtho( 0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 600 ); // essentially set coordinate system
+    glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
+    glLoadIdentity( ); // same as above comment
+
+    GLfloat halfScreenWidth = SCREEN_WIDTH / 2;
+    GLfloat halfScreenHeight = SCREEN_HEIGHT / 2;
+
+    GLfloat halfSideLength = 200;
+
+    GLfloat vertices[] =
+            {
+                    halfScreenWidth, halfScreenHeight + halfSideLength, 0.0, // top center vertex
+                    halfScreenWidth - halfSideLength, halfScreenHeight - halfSideLength, 0.0, // bottom left corner
+                    halfScreenWidth + halfSideLength, halfScreenHeight - halfSideLength, 0.0 // bottom right corner
+            };
+
+    GLfloat colour[] =
+            {
+                    255, 0, 0,
+                    0, 255, 0,
+                    0, 0, 255
+            };
+
+    // Loop until the user closes the window
+    while ( !glfwWindowShouldClose( window ) )
+    {
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        // Render OpenGL here
+        glEnableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're using a vertex array for fixed-function attribute
+        glEnableClientState( GL_COLOR_ARRAY );
+        glVertexPointer( 3, GL_FLOAT, 0, vertices ); // point to the vertices to be used
+        glColorPointer( 3, GL_FLOAT, 0, colour );
+        glDrawArrays( GL_TRIANGLES, 0, 3 ); // draw the vertixes
+        glDisableClientState( GL_COLOR_ARRAY );
+        glDisableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're finished using the vertex array attribute
+
+
+
+        // Swap front and back buffers
+        glfwSwapBuffers( window );
+
+        // Poll for and process events
+        glfwPollEvents( );
+    }
+
+    glfwTerminate( );
+
+    return 0;
 }
 
-// input keyboard function to quit with q or Q.
-void quit(unsigned char key, int x, int y)
-{
-  if (key == 'q' || key == 'Q')
-    exit(0);
-}
 
-int main (int argc, char** argv)
-{
-  glutInit(&argc, argv);        // initialize glut
-  glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE); // RGB, single buffer mode
-  glutInitWindowSize(1280,720);  // set window width, height in pixels
-  glutInitWindowPosition(0,0);  // x, y from top left corner of screen
-  glutCreateWindow(argv[0]);    // create window, with program name in titlebar
-  glutDisplayFunc(sendHELP);     // set display callback function
-  glutKeyboardFunc(quit);       // set keyboard callback function
-  glutMainLoop();               // hand over control to glut
-  return 0;
-}
+
